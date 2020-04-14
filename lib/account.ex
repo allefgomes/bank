deedae96bbb06c7087d8db3431d5b7d6458197ec
Allef Gomes <allefgalmeida@gmail.com>
2020-04-14 20:15:33 -0300

    Create transfer and validate_balance  methods

--- lib/account.ex
+++ lib/account.ex
@@ -4,6 +4,23 @@ defmodule Account do
   # inline method
   def create(user), do: %__MODULE__{user: user}
 
+  def transfer(accounts, account_of, account_to, value) do
+    account_of = get_user(accounts, account_of)
+
+    cond do
+      validate_balance(account_of.balance, value) -> {:error, "Insufficient funds"}
+
+      true ->
+        account_to = get_user(accounts, account_to)
+
+        account_of = %Account{account_of | balance: account_of.balance - value}
+        account_to = %Account{account_to | balance: account_to.balance + value}
+
+        [account_of, account_to]
+    end
+  end
+
+  defp validate_balance(balance, value_to_transfer), do: balance < value_to_transfer
 
   defp get_user(accounts, account_user), do: Enum.find(accounts, fn account -> account.user.email == account_user.user.email end)
 end
