8bdf8c1a7941f14f65fbef32945386f41f939028
Allef Gomes <allefgalmeida@gmail.com>
2020-04-18 09:55:30 -0300

    Write accounts in document

--- lib/account.ex
+++ lib/account.ex
@@ -1,17 +1,39 @@
 defmodule Account do
   defstruct user: User, balance: 1000
 
+  @accounts_file "accounts.txt"
   # inline method
-  def create(user), do: %__MODULE__{user: user}
+  def create(user) do
+    # TODO: Check validate
+    search_by_email(user.email)
 
-  def transfer(accounts, account_of, account_to, value) do
-    account_of = get_user(accounts, account_of)
+    binary = [%__MODULE__{user: user}] ++ search()
+    |> :erlang.term_to_binary
+
+    File.write(@accounts_file, binary)
+  end
+
+  def search() do
+    {:ok, binary} = File.read(@accounts_file)
+    :erlang.binary_to_term(binary)
+  end
+
+  defp search_by_email(email) do
+    Enum.find(
+      search(),
+      &(&1.user.email == email) # It's a anonimous function
+      # fn account -> account.user.email == email end
+    )
+  end
+
+  def transfer(account_of, account_to, value) do
+    account_of = search_by_email(account_of.user.email)
 
     cond do
       validate_balance(account_of.balance, value) -> {:error, "Insufficient fundsto transfer!"}
 
       true ->
-        account_to = get_user(accounts, account_to)
+        account_to = search_by_email(account_to.user.email)
 
         account_of = %Account{account_of | balance: account_of.balance - value}
         account_to = %Account{account_to | balance: account_to.balance + value}
@@ -30,6 +52,4 @@ defmodule Account do
   end
 
   defp validate_balance(balance, value_to_transfer), do: balance < value_to_transfer
-
-  defp get_user(accounts, account_user), do: Enum.find(accounts, fn account -> account.user.email == account_user.user.email end)
 end
